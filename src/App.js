3199d9d102f01ebb08ab2ed7fc012dd69b1f592a
oleynsve <oleynsve@gmail.com>
2022-07-05 21:26:23 +0200

    Some changes

--- src/App.js
+++ src/App.js
@@ -162,8 +162,8 @@ function App() {
 
   const incrementMintAmount = () => {
     let newMintAmount = mintAmount + 1;
-    if (newMintAmount > 50) {
-      newMintAmount = 50;
+    if (newMintAmount > 20) {
+      newMintAmount = 20;
     }
     setMintAmount(newMintAmount);
   };
